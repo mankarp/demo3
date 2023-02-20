907b5040de8f1f407592ce3dbd377a0e7074490e
Poonam <poonammankar93@gmail.com>
2023-02-20 15:40:31 +0530

    git demo3 commit.

---
+++ src/Arrays_program/Fibonancy.java
@@ -0,0 +1,16 @@
+package Arrays_program;
+
+public class Fibonancy {
+
+	public static void main(String[] args) {
+
+		int a=1,b=1,c;
+		for(int i=0; i<=10; i++) {
+			c=a+b;
+			System.out.println(b);
+			a=b;
+			b=c;
+		}
+	}
+
+}
