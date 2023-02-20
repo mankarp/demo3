123cf1945cac68b1458029baae63fdbb5a809b1f
mankarp <125647909+mankarp@users.noreply.github.com>
2023-02-20 16:13:25 +0530

    no change
    100

--- src/Arrays_program/Fibonancy.java
+++ src/Arrays_program/Fibonancy.java
@@ -5,7 +5,7 @@ public class Fibonancy {
 	public static void main(String[] args) {
 
 		int a=1,b=1,c;
-		for(int i=0; i<=20; i++) {
+		for(int i=0; i<=100; i++) {
 			c=a+b;
 			System.out.println(b);
 			a=b;
